7c2effe2bd856caddd97d0e1d625edfab9a507f1
YazanAlhalabi <yazanalhalabi1@gmail.com>
2023-03-17 16:18:59 +0200

    added prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ function App() {
     <div className="App">
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
     </div>
   )
