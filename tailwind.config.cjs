cc6377eb5fd76092f18cfb5d6f226860f5d45cf8
uch2ha <sinyavskiy.d@gmail.com>
2023-03-25 12:27:24 +0200

    fix bg not working after building

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-        'home-page-bg': "url('./src/assets/homePageBg.jpg')",
-        'home-page-bg-test': "url('./src/assets/bg-test.webp')"
+        'home-page-bg': "url('./assets/homePageBg.jpg')",
+        'home-page-bg-test': "url('./assets/bg-test.webp')"
       }
     }
   },
