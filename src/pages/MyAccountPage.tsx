e55a527c6e40e4dc1d585fada51467cb205f8171
uch2ha <sinyavskiy.d@gmail.com>
2023-03-26 10:13:31 +0300

    new pages inited

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react'
+import NavBar from '../components/NavBar'
 
 const MyAccountPage: FC = () => {
-  return <div>MyAccountPage</div>
+  return (
+    <div className="bg-gray-400 w-screen h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center  justify-start">
+      <NavBar />
+      <div className=" h-full w-full flex justify-center items-center text-center flex-col">
+        <p>MyAccountPage</p>
+      </div>
+    </div>
+  )
 }
 
 export default MyAccountPage
