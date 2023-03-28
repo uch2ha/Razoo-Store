05f06028d71a63e0b5483608367251305fe1d53e
uch2ha <sinyavskiy.d@gmail.com>
2023-03-28 17:10:24 +0300

    admin components created

---
+++ src/features/admin/components/AdminProducts.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const AdminProducts: FC = () => {
+  return <div>AdminProducts</div>
+}
+
+export default AdminProducts
---
+++ src/features/admin/components/AdminUsers.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const AdminUsers: FC = () => {
+  return <div>AdminUsers</div>
+}
+
+export default AdminUsers
