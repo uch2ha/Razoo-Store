8c6c719afe461247cd6ae05f49445dc2b3e922b3
uch2ha <sinyavskiy.d@gmail.com>
2023-03-29 22:26:13 +0300

    added product img to orders page

--- src/features/myAccount/components/orders/OrderDetailsRow.tsx
+++ src/features/myAccount/components/orders/OrderDetailsRow.tsx
@@ -16,10 +16,19 @@ const OrderDetailsRow: FC<IOrderDetailsRowProps> = ({ itemId, amount }) => {
   const product = getProductById(itemId)
   return (
     <>
-      <div className="col-span-3 border-b-2 py-5">{product?.name}</div>
-      <div className="col-span-2 border-b-2 border-l-2 border-r-2 py-5">{product?.size}</div>
-      <div className="col-span-2 border-b-2  border-r-2 py-5">{amount}X</div>
-      <div className="col-span-2 border-b-2 py-5">{product?.price} $</div>
+      <div className="col-span-3 border-b-2 py-5 flex justify-start items-center">
+        <img src={`./src/assets/productImg/${product?.img}.png`} className="w-[30%]" />
+        <p>{product?.name}</p>
+      </div>
+      <div className="col-span-2 border-b-2 border-l-2 border-r-2 py-5 flex justify-center items-center">
+        <p>{product?.size}</p>
+      </div>
+      <div className="col-span-2 border-b-2  border-r-2 py-5 flex justify-center items-center">
+        <p>{amount}X</p>
+      </div>
+      <div className="col-span-2 border-b-2 py-5 flex justify-center items-center">
+        <p>{product?.price} $</p>
+      </div>
     </>
   )
 }
