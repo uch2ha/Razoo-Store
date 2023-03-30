5bbd69fae7930d230efba0e45c24f00f0708a227
uch2ha <sinyavskiy.d@gmail.com>
2023-03-30 15:13:42 +0300

    new product img added to main page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react'
 import NavBar from '../components/NavBar'
 
-import shampoo from '../assets/productImg/1.png'
-
 const HomePage: FC = () => {
   return (
     <div className="bg-bg2 w-screen h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center text-white justify-start">
       <NavBar />
       <div className="relative text-5xl h-full w-full flex justify-center items-center text-center flex-col ">
-        <img className="absolute top-[20%] rotate-12 w-[500px] " src={shampoo} />
+        <img
+          className="absolute top-[17%] rotate-10 w-[900px] "
+          src="./src/assets/mainPageProductImg.png"
+        />
         <div className="absolute top-[20%]">
           <p>100% organic</p>
           <p>WITHOUT</p>
