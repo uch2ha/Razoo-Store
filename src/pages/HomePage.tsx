d9bae20b49fb87556661f34a0913da3c7e267f3d
uch2ha <sinyavskiy.d@gmail.com>
2023-03-31 10:57:33 +0300

    main img size changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage: FC = () => {
     <div className="bg-mainPageBg w-screen h-screen bg-cover bg-center bg-no-repeat flex flex-col items-center justify-start text-white">
       <NavBar isWhite={true} />
       <div className="relative text-5xl  h-full w-full flex justify-center items-center text-center flex-col ">
-        <img className="absolute top-[20%] rotate-10 w-[500px]" src={productImg} />
+        <img className="absolute top-[22%] rotate-10 w-[480px]" src={productImg} />
         <div className="absolute top-[15%]">
           <p className="font-poppins text-xl mb-2">100% organic</p>
           <p className="font-garamond text-[83px] mb-1">WITHOUT</p>
