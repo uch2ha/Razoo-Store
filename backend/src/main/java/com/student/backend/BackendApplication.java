4d451a6e717124fb05373a62aca663b3f448b101
uch2ha <sinyavskiy.d@gmail.com>
2023-05-04 12:20:53 +0300

    init

--- backend/src/main/java/com/student/backend/BackendApplication.java
+++ backend/src/main/java/com/student/backend/BackendApplication.java
@@ -10,7 +10,7 @@ public class BackendApplication
   public static void main(String[] args)
   {
     SpringApplication.run(BackendApplication.class, args);
-    System.out.println(21asdsa3);
+    System.out.println(2112321);
   }
 
 }
