407bbf5f137b4d9950de02f58467e721de0dc303
uch2ha <sinyavskiy.d@gmail.com>
2023-05-07 15:34:01 +0300

    file renaming

--- backend/src/main/java/com/student/backend/App.java
+++ backend/src/main/java/com/student/backend/App.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication
+public class App
 {
 
   public static void main(String[] args)
   {
-    SpringApplication.run(BackendApplication.class, args);
+    SpringApplication.run(App.class, args);
   }
 
 }
