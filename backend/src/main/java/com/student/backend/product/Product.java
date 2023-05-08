b874c6b033fd8e59632a623889cb5e5e901f9779
uch2ha <sinyavskiy.d@gmail.com>
2023-05-08 13:43:59 +0300

    Product class ready, validation annotations added

--- backend/src/main/java/com/student/backend/product/Product.java
+++ backend/src/main/java/com/student/backend/product/Product.java
@@ -1,10 +1,11 @@
 package com.student.backend.product;
 
 import com.student.backend.product.Emuns.Category;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,12 +26,36 @@ public class Product
   @GeneratedValue
   private UUID productId;
 
+  @Column(nullable = false)
+  @NotBlank(message = "Product name is mandatory")
+  @Size(min = 3, max = 50, message = "Product name must be between 3 and 50 characters")
   private String name;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Description is mandatory")
+  @Size(min = 3, max = 200, message = "Description must be between 3 and 200 characters")
   private String description;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Instruction is mandatory")
+  @Size(min = 3, max = 100, message = "Instruction must be between 3 and 100 characters")
   private String instruction;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Size is mandatory")
+  @Size(min = 3, max = 10, message = "Size must be between 3 and 10 characters")
   private String size;
-  private Category category;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Price is mandatory")
+  @Min(value = 0L, message = "Price must be greater than zero")
+  @Max(value = 100_000L, message = "Price mustn't be greater than 100_000")
   private int price;
+
+  @Column(nullable = false)
+  @NotBlank(message = "Category is mandatory")
+  private Category category;
+
   private LocalDateTime createdAt = LocalDateTime.now();
 
   //todo add IMG column/link
