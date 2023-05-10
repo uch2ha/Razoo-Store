d9de017833be49f685ce4ed3bb3a8abcf938d8f5
uch2ha <sinyavskiy.d@gmail.com>
2023-05-10 19:25:17 +0300

    Created init SQL file

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO _user (user_id, first_name, last_name, email, password, is_google_login)
+VALUES ('835549f1-53c8-43df-b5c2-04bbbef1aee5', 'Dmitry', 'Sinyavskiy', 'Dima@dima.com', 'admin',
+        false);
