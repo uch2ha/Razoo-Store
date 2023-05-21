c5f292a272065d6a955a97a26452176ad4a8cf42
uch2ha <sinyavskiy.d@gmail.com>
2023-05-21 13:18:22 +0300

    new cors @Bean added

--- backend/src/main/java/com/student/backend/config/CorsConfig.java
+++ backend/src/main/java/com/student/backend/config/CorsConfig.java
@@ -1,6 +1,12 @@
 package com.student.backend.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +21,21 @@ public class CorsConfig implements WebMvcConfigurer
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
             .allowedHeaders("*")
             .exposedHeaders("Access-Control-Allow-Origin");
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean()
+  {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+
+    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
