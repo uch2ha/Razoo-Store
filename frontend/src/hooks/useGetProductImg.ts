51fefd4121eadbac46602594cef7b366d358a34d
uch2ha <sinyavskiy.d@gmail.com>
2023-05-25 14:16:27 +0300

    FIX FE deploy err

--- frontend/src/hooks/useGetProductImg.ts
+++ frontend/src/hooks/useGetProductImg.ts
@@ -1,16 +1,18 @@
 import axios from 'axios'
 
-export const useGetProductImg = async (productId: string) => {
-  const res = await axios.get(`http://localhost:8080/api/v1/products/${productId}/product-image`, {
-    responseType: 'blob'
-  })
+const URL = import.meta.env.VITE_BASE_URL + import.meta.env.VITE_API_VERSION
 
-  if (res.status === 200) {
-    if (res.data?.size !== 0) {
-      return URL.createObjectURL(res.data)
+export const useGetProductImg = async (productId: string) => {
+    const res = await axios.get(`${URL}/products/${productId}/product-image`, {
+        responseType: 'blob'
+    })
+
+    if (res.status === 200) {
+        if (res.data?.size !== 0) {
+            return URL.createObjectURL(res.data)
+        }
+    } else {
+        console.log(res)
+        throw new Error('Failed to fetch product image')
     }
-  } else {
-    console.log(res)
-    throw new Error('Failed to fetch product image')
-  }
 }
