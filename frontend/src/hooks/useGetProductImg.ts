f786338a19cc9ec0f286f5a6c3d2dc7c94450b0f
uch2ha <sinyavskiy.d@gmail.com>
2023-05-25 14:25:34 +0300

    fix img not worked

--- frontend/src/hooks/useGetProductImg.ts
+++ frontend/src/hooks/useGetProductImg.ts
@@ -3,16 +3,16 @@ import axios from 'axios'
 const URL = import.meta.env.VITE_BASE_URL + import.meta.env.VITE_API_VERSION
 
 export const useGetProductImg = async (productId: string) => {
-    const res = await axios.get(`${URL}/products/${productId}/product-image`, {
-        responseType: 'blob'
-    })
+  const res = await axios.get(`${URL}/products/${productId}/product-image`, {
+    responseType: 'blob'
+  })
 
-    if (res.status === 200) {
-        if (res.data?.size !== 0) {
-            return URL.createObjectURL(res.data)
-        }
-    } else {
-        console.log(res)
-        throw new Error('Failed to fetch product image')
+  if (res.status === 200) {
+    if (res.data?.size !== 0) {
+      return URL.createObjectURL(res.data)
     }
+  } else {
+    console.log(res)
+    throw new Error('Failed to fetch product image')
+  }
 }
