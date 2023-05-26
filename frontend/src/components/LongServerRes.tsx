0f32413410d1bb0c540e4fdb6d63da17012a61cd
uch2ha <sinyavskiy.d@gmail.com>
2023-05-26 16:09:51 +0300

    added msg about long connection to server

---
+++ frontend/src/components/LongServerRes.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LongServerRes = () => {
+  return (
+    <div className="col-span-full mx-auto h-fit text-2xl py-2 px-6">
+      <p>Trying to connect to server</p>
+      <p>While first time, may take some time (3-5min)</p>
+      <p>I apologize for the inconvenience</p>
+    </div>
+  )
+}
+
+export default LongServerRes
